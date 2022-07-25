5422a48f1524d59c4aa372975fe02e85bdf261ad
MarcosMartilotta <MarcosMartilotta@users.noreply.github.com>
2022-07-25 02:36:29 +0000

    deploy: 080600dee781a93fe03a6a3577e9768302068c1e

---
+++ _dist_/index.js
@@ -0,0 +1,52 @@
+/**
+ * This file is just a silly example to show everything working in the browser.
+ * When you're ready to start on your site, clear the file. Happy hacking!
+ **/
+
+console.log('Happy hacking :)')
+
+const baseUrl = 'https://platzi-avo.vercel.app';
+const appNode =document.querySelector('#app')
+
+
+//intl
+//1--format-monedas
+
+const formatPrice = (price) => {
+    const newPrice = new window.Intl.NumberFormat('en-EN', {
+        style: 'currency',
+        currency: 'USD',
+    }).format(price);
+    return price;
+}
+
+
+
+//web api
+// Conectarnos al server
+window.fetch(`${baseUrl}/api/avo`)
+//procesar la respuesta y convertirla en JSON
+    .then((respuesta) => respuesta.json())
+//JSON -> Data -> REnderizar infro browser
+    .then((responseJson) => {
+        const todosLosItems = [];
+        responseJson.data.forEach((item) => {
+            //Creo una imagen
+            const imagen = document.createElement('img');
+            imagen.src = `${baseUrl}${item.image}`
+            //Crear titulo
+            const title = document.createElement('h2');
+            title.textContent = item.name;
+            title.className = 'muy-grande'; //como uso tailwind puedo poner 'text-2xl' y ya le aplica una letra grande. Tambien puedo ir pegando varias clases de tailwind para darle estilo.
+            //crear precio 
+            const price = document.createElement('div');
+            price.textContent = formatPrice(item.price);
+
+            const container = document.createElement('div');
+            container.append(imagen, title, price)
+
+            todosLosItems.push(container);
+        });
+
+        appNode.append(...todosLosItems);
+    }) 
